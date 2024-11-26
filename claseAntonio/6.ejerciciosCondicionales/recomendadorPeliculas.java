af69b101619ef3ba5c7705e5aae9495f34b31c88
ManuelCaroDev <manuelcaro.dev@gmail.com>
2024-11-26 21:47:07 +0100

    0010

---
+++ claseAntonio/6.ejerciciosCondicionales/recomendadorPeliculas.java
@@ -0,0 +1,20 @@
+package claseAntonio.6.ejerciciosCondicionales;
+
+import java.util.Scanner;
+
+public class recomendadorPeliculas {
+
+    public static void main(String[] args) {
+        //definimos nuestros parametros o factores con los que recomendar una pelicula
+        String genero = "Terror";
+        int duracion = 95;
+
+
+        //condicional/condicionales
+        if(genero.equals(genero)) {
+
+        }
+    }
+
+
+}
